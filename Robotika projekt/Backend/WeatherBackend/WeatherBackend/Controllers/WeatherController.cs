947bd94c698181af7b181a964687e834f0cc8112
Istok03 <szilagyikristof51@gmail.com>
2025-12-07 11:58:50 +0100

    Backend / API
    Backend és api csinálgatások

---
+++ Robotika projekt/Backend/WeatherBackend/WeatherBackend/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using WeatherBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] WeatherData data)
+        {
+            Console.WriteLine($"Temp: {data.Tempature}, Humidity: {data.Humidity}, Pressure: {data.Pressure}");
+            return Ok(new { status = "recived"});
+        }
+       
+
+
+    }
+}
